d28c4726a847c43cd01c12534cbaf4e73b2b7d6e
ekaterina <3k4t3r1na.l30n0v4@gmail.com>
2024-08-07 21:12:51 -0700

    commit1

---
+++ app.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'Hello World'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
